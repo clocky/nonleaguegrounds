39a421c3f12217cbec3239f9725efb7d67caf17b
Mark McLaughlin <mark@mclaughlin.me.uk>
2022-10-29 21:20:59 -0700

    chore: add bing maps api key

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,7 +1,8 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = function () {
   return {
     google_maps_api_key: process.env.GOOGLE_MAPS_API_KEY,
+    bing_maps_api_key: process.env.BING_MAPS_API_KEY,
   };
 };
