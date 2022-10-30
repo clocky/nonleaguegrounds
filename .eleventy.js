83da556bc4fb66e603ca3523e7aeb614b8cd7554
Mark McLaughlin <mark@mclaughlin.me.uk>
2022-10-29 21:18:47 -0700

    feat: distance to nearby clubs

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ const yaml = require("js-yaml");
 const ordinal = require("ordinal");
 const commaNumber = require("comma-number");
 const EleventyFetch = require("@11ty/eleventy-fetch");
+const distFrom = require("distance-from");
 
 module.exports = function (eleventyConfig) {
   /** Add loader for YAML files */
@@ -26,20 +27,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("ordinal", (num) => ordinal(num));
   eleventyConfig.addFilter("commaNumber", (num) => commaNumber(num));
 
-  eleventyConfig.addAsyncShortcode("latitude", async function (postcode) {
-    const fetch = await fetchPostcode(postcode);
-    if (!fetch) {
-      return "";
-    }
-    return `${fetch.result.latitude}`;
-  });
-
-  eleventyConfig.addAsyncShortcode("longitude", async function (postcode) {
-    const fetch = await fetchPostcode(postcode);
-    if (!fetch) {
-      return "";
-    }
-    return `${fetch.result.longitude}`;
+  eleventyConfig.addShortcode("distance", (a, b, x, y) => {
+    let distance = new distFrom([a, b]).to([x, y]).in("mi");
+    return distance.toFixed(1);
   });
 
   eleventyConfig.addTransform("prettier", function (content, outputPath) {
@@ -47,12 +37,13 @@ module.exports = function (eleventyConfig) {
     switch (extname) {
       case ".html":
         const parser = extname.replace(/^./, "");
-        return prettier.format(content, { parser });
+        return prettier.format(content, { printWidth: 512, parser: parser });
 
       default:
         return content;
     }
   });
+
   return {
     dir: {
       input: "src",
@@ -60,14 +51,3 @@ module.exports = function (eleventyConfig) {
     },
   };
 };
-
-async function fetchPostcode(postcode) {
-  if (!postcode) {
-    return;
-  }
-  const url = `https://api.postcodes.io/postcodes/${postcode}`;
-  return EleventyFetch(url, {
-    duration: "90d", // 90 days
-    type: "json",
-  });
-}
