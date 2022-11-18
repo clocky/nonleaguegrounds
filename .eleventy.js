842b22b924ed32169fe5f7d2f80c282862c22c40
Mark McLaughlin <mark@mclaughlin.me.uk>
2022-11-17 18:44:27 -0800

    Remore migrated data API shortcodes

--- .eleventy.js
+++ .eleventy.js
@@ -32,70 +32,7 @@ module.exports = function (eleventyConfig) {
     return distance.toFixed(1);
   });
 
-  eleventyConfig.addAsyncShortcode("county", async function (postcode) {
-    const response = await postcodesIO(postcode);
-    let county = "";
-    if (response && response.status === 200) {
-      if (response.result.admin_county != null) {
-        county = response.result.admin_county;
-      }
-    }
-    return county;
-  });
-
-  eleventyConfig.addAsyncShortcode("country", async function (postcode) {
-    const response = await postcodesIO(postcode);
-    let country = "";
-    let ISO3166 = "";
-    if (response && response.status === 200) {
-      if (response.result.country != null) {
-        country = response.result.country;
-      }
-    }
-    /* https://www.gov.uk/government/publications/open-standards-for-government/country-codes */
-    switch (country) {
-      case "England":
-        ISO3166 = "GB-ENG";
-        break;
-      case "Wales":
-        ISO3166 = "GB-WLS";
-        break;
-      case "Scotland":
-        ISO3166 = "GB-SCT";
-        break;
-      case "Northern Ireland":
-        ISO3166 = "GB-NIR";
-        break;
-      default:
-        ISO3166 = "GB";
-    }
-    return ISO3166;
-  });
-
-  eleventyConfig.addAsyncShortcode("region", async function (postcode) {
-    postcode = postcode.replace(/\s/g, "");
-    const response = await postcodesIO(postcode);
-    let region = "";
-    if (response && response.status === 200) {
-      if (response.result.region != null) {
-        region = response.result.region;
-      } else {
-        /* Backup for Welsh teams */
-        region = response.result.country;
-      }
-    }
-    return region;
-  });
-
-  eleventyConfig.addAsyncShortcode("district", async function (postcode) {
-    const response = await postcodesIO(postcode);
-    let district = "";
-    if (response && response.status === 200) {
-      district = response.result.admin_district;
-    }
-    return district;
-  });
-
+  
   eleventyConfig.addTransform("prettier", function (content, outputPath) {
     const extname = path.extname(outputPath);
     switch (extname) {
