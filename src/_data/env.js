cd86a97a873178af252b4ae618140fc168a98612
Mark McLaughlin <mark@mclaughlin.me.uk>
2022-11-26 15:49:03 -0800

    Expose Eleventy prod or dev environment to templates

--- src/_data/env.js
+++ src/_data/env.js
@@ -4,5 +4,6 @@ module.exports = function () {
   return {
     google_maps_api_key: process.env.GOOGLE_MAPS_API_KEY,
     bing_maps_api_key: process.env.BING_MAPS_API_KEY,
+    eleventy: process.env.ELEVENTY_ENV,
   };
 };
