ebb62f527d723260e82a640e8651d32b81aa12b9
Mark McLaughlin <mark@mclaughlin.me.uk>
2022-12-14 09:31:26 -0800

    fix: Use Eleventy-Fetch for external tweet assets

--- .eleventy.js
+++ .eleventy.js
@@ -10,8 +10,8 @@ const brokenLinksPlugin = require("eleventy-plugin-broken-links");
 const Image = require("@11ty/eleventy-img");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const slugify = require("slugify");
-const fetch  = require("node-fetch");
-require('dotenv').config();
+const EleventyFetch = require("@11ty/eleventy-fetch");
+require("dotenv").config();
 const GOOGLE_MAPS_KEY = process.env.GOOGLE_MAPS_API_KEY;
 
 module.exports = function (eleventyConfig) {
@@ -32,15 +32,15 @@ module.exports = function (eleventyConfig) {
        All of these 'fail' due to 429 rate limit or 302 moved errors
        but still have valid content.
     */
-    excludeUrls: [ 
-     "https://maps.apple.com/*",          // Redirects to /place?q and 404's
-     "https://www.facebook.com/*",        // Gives a 302 error with Location:
-     "https://www.youtube.com/channel/*", 
-     "https://twitter.com/*",
-     "https://soundcloud.com/*",          // Gives a 302 error
-     "https://www.instagram.com/*",       // Gives a 429 error
-     "https://altrinchamfc.com/*",       // Avoid cdn.shopify.com error
-     "https://service.gov.uk/*",        //  Claims "INVALID URI" but works
+    excludeUrls: [
+      "https://maps.apple.com/*", // Redirects to /place?q and 404's
+      "https://www.facebook.com/*", // Gives a 302 error with Location:
+      "https://www.youtube.com/channel/*",
+      "https://twitter.com/*",
+      "https://soundcloud.com/*", // Gives a 302 error
+      "https://www.instagram.com/*", // Gives a 429 error
+      "https://altrinchamfc.com/*", // Avoid cdn.shopify.com error
+      "https://service.gov.uk/*", //  Claims "INVALID URI" but works
     ],
   });
 
@@ -54,7 +54,9 @@ module.exports = function (eleventyConfig) {
    * Pass through copy
    */
 
-  eleventyConfig.addPassthroughCopy({ "node_modules/@fortawesome/fontawesome-free/webfonts": "webfonts" });
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/@fortawesome/fontawesome-free/webfonts": "webfonts",
+  });
   eleventyConfig.addPassthroughCopy("./src/js/*.js");
   eleventyConfig.addPassthroughCopy({ "src/static": "/" });
 
@@ -77,29 +79,43 @@ module.exports = function (eleventyConfig) {
     return distance.toFixed(1);
   });
 
-  eleventyConfig.addShortcode("staticmap", function (address, width = 500, height = 500, zoom = 13, scale = 1, maptype = "roadmap") {
-    const google = new URL("https://maps.googleapis.com/maps/api/staticmap");
-    google.searchParams.set("center", address);
-    google.searchParams.set("size", `${width}x${height}`);
-    google.searchParams.set("zoom", zoom);
-    google.searchParams.set("scale", scale);
-    google.searchParams.set("maptype", maptype);
-    google.searchParams.set("key", GOOGLE_MAPS_KEY);
-		return google;
-  });
-  
-  eleventyConfig.addNunjucksAsyncShortcode("tweet",
-    async function (url) {
-      console.log(`Retrieiving tweet from ${url.substring(0, url.length - 1)}`)
-      const twitter = new URL("https://publish.twitter.com/oembed");
-      twitter.searchParams.set("url", url.substring(0, url.length - 1));
-      twitter.searchParams.set("limit", 1);
-      twitter.searchParams.set("lang", "en");
-      twitter.searchParams.set("chrome", "noborders nofooter noheader");
-      let { html } = await fetch(twitter).then((res) => res.json());
-      return html;
-    });
+  eleventyConfig.addShortcode(
+    "staticmap",
+    function (
+      address,
+      width = 500,
+      height = 500,
+      zoom = 13,
+      scale = 1,
+      maptype = "roadmap"
+    ) {
+      const google = new URL("https://maps.googleapis.com/maps/api/staticmap");
+      google.searchParams.set("center", address);
+      google.searchParams.set("size", `${width}x${height}`);
+      google.searchParams.set("zoom", zoom);
+      google.searchParams.set("scale", scale);
+      google.searchParams.set("maptype", maptype);
+      google.searchParams.set("key", GOOGLE_MAPS_KEY);
+      return google;
+    }
+  );
 
+  eleventyConfig.addNunjucksAsyncShortcode("tweet", async function (tweet) {
+    const url = new URL("https://publish.twitter.com/oembed");
+    url.searchParams.set("url", tweet.substring(0, tweet.length - 1));
+    url.searchParams.set("limit", 1);
+    url.searchParams.set("lang", "en");
+    url.searchParams.set("chrome", "noborders nofooter noheader");
+    url.searchParams.set("dnt", true);
+    url.searchParams.set("hide_thread", true);
+
+    const response = await EleventyFetch(url.href, {
+      duration: "1d", 
+      type: "json",
+    })
+
+    return response.html;
+  });
 
   /** Add a filter to format inline dates for <time> tags */
   let yearsAgo = (year) => dayjs().diff(dayjs(year, "YYYY"), "year");
@@ -149,9 +165,9 @@ async function imageShortcode(src, widths, formats, alt, sizes, urlPath) {
     outputDir: `dist/img/${urlPath}`,
     formats: formats,
     cacheDuration: "1y",
-    filenameFormat: function (id, src, width, format, options, alt) {;
+    filenameFormat: function (id, src, width, format, options, alt) {
       return `${slug}-${width}w.${format}`;
-    }
+    },
   });
 
   let imageAttributes = {
