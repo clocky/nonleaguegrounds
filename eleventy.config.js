5845ba5a08eca8e463edc1e009ef0d84e0087020
Mark McLaughlin <mark@mclaughlin.me.uk>
2023-04-09 22:05:24 -0700

    Import WebC plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,10 +4,15 @@ const ordinal = require("ordinal");
 const commaNumber = require("comma-number");
 const distFrom = require("distance-from");
 const eleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
 
+/* Additional Vite plugin for image optimization */
 const { ViteImageOptimizer } = require("vite-plugin-image-optimizer");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyWebcPlugin, {
+    components: "src/components/**/*.webc",
+  });
   /** Load Vite plugin */
   eleventyConfig.addPlugin(eleventyVitePlugin, {
     viteOptions: {
@@ -49,9 +54,14 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("date", (date, format) =>
     dayjs(date).format(format)
   );
+
+  /* Filter to convert integer to ordinal, ie. 2 = 2nd */
   eleventyConfig.addFilter("ordinal", (num) => ordinal(num));
+
+  /* Filter to convert integer to comma number, ie. 1000 = 1,000 */
   eleventyConfig.addFilter("commaNumber", (num) => commaNumber(num));
 
+  /* Filter to calculate distance between two points */
   eleventyConfig.addShortcode("distance", (a, b, x, y) => {
     let distance = new distFrom([a, b]).to([x, y]).in("mi");
     return distance.toFixed(1);
