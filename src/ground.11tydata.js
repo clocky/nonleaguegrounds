3d013287229dce20b1adba9d947d3d2c44cd629d
Mark McLaughlin <mark@mclaughlin.me.uk>
2023-04-13 13:18:57 -0700

    Use 11tydata to create dynamic permalinks

--- src/ground.11tydata.js
+++ src/ground.11tydata.js
@@ -1,10 +1,12 @@
 const slugify = require("slugify");
 
 module.exports = {
-  layout: "base",
+  layout: "base.webc",
   tags: ["ground"],
   eleventyComputed: {
     permalink: ({ ground }) =>
-      `regions/${slugify(ground.areaServed.name.toLowerCase())}/${slugify(ground.name.toLowerCase())}/index.html`,
+      `regions/${slugify(ground.areaServed.name.toLowerCase())}/${slugify(
+        ground.name.toLowerCase()
+      )}/index.html`,
   },
 };
