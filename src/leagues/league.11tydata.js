15a165bee797f33be04b3c330a7acd61a052dd9b
Mark McLaughlin <mark@mclaughlin.me.uk>
2023-04-24 10:07:34 -0700

    Use Sanity slug instead of generating our own

--- src/leagues/league.11tydata.js
+++ src/leagues/league.11tydata.js
@@ -4,7 +4,6 @@ module.exports = {
   layout: "base.webc",
   tags: ["league"],
   eleventyComputed: {
-    permalink: ({ league }) =>
-      `leagues/${slugify(league.name.toLowerCase())}/index.html`,
+    permalink: ({ league }) => `leagues/${league.slug.current}/index.html`,
   },
 };
