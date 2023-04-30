0ca8545a38b0e5a1333b9a162fa5400197ddf0a3
Mark McLaughlin <mark@mclaughlin.me.uk>
2023-04-30 10:58:21 -0700

    Generate titles at build time

--- src/leagues/club.11tydata.js
+++ src/leagues/club.11tydata.js
@@ -4,6 +4,7 @@ module.exports = {
   layout: "base.webc",
   tags: ["club"],
   eleventyComputed: {
+    title: ({ club }) => club.name,
     permalink: ({ club }) =>
       `leagues/${club.memberOf.slug.current}/${club.slug.current}/index.html`,
   },
