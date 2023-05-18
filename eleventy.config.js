c8a830de022d8c5fcc8e9ce757c023ea471ae1b5
Mark McLaughlin <mark@mclaughlin.me.uk>
2023-05-18 08:46:04 -0700

    chore: remoe unused packages

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,15 +2,14 @@ const dayjs = require("dayjs")
 const yaml = require("js-yaml")
 const ordinal = require("ordinal")
 const commaNumber = require("comma-number")
-const distFrom = require("distance-from")
-const eleventyVitePlugin = require("@11ty/eleventy-plugin-vite")
 const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc")
 const { eleventyImagePlugin } = require("@11ty/eleventy-img")
 const htmlmin = require("html-minifier")
+const converter = require("number-to-words")
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyWebcPlugin, {
-    components: ["src/components/**/*.webc", "npm:@11ty/eleventy-img/*.webc"],
+    components: ["src/_components/**/*.webc", "npm:@11ty/eleventy-img/*.webc"],
   })
 
   eleventyConfig.addPlugin(eleventyImagePlugin, {
@@ -30,7 +29,7 @@ module.exports = function (eleventyConfig) {
         removeComments: true,
         collapseWhitespace: false,
         preserveLineBreaks: true,
-        removeRedundantAttributes: true,
+        removeRedundantAttributes: false,
         removeEmptyAttributes: true,
         removeEmptyElements: false,
         collapseBooleanAttributes: true,
@@ -41,61 +40,31 @@ module.exports = function (eleventyConfig) {
     return content
   })
 
-  /** Load Vite plugin */
-  eleventyConfig.addPlugin(eleventyVitePlugin, {
-    viteOptions: {
-      assetsInclude: ["**/*.manifest"],
-      publicDir: "src/public",
-      build: {
-        mode: "production",
-        sourcemap: false,
-        manifest: true,
-        target: "node18.15.0",
-        rollupOptions: {
-          output: {
-            dir: "dist",
-            assetFileNames: (asset) => {
-              if (/\.css$/.test(asset.name)) {
-                return "assets/css/[name]-[hash][extname]"
-              }
-              return "assets/images/[name]-[hash][extname]"
-            },
-            chunkFileNames: "assets/js/[name]-[hash].js",
-            entryFileNames: "assets/js/main.js",
-          },
-        },
-      },
-      plugins: [],
-    },
-  })
-
   /** Add loader for YAML files */
   eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents))
 
   /** Add passthrough copy for static assets */
-  eleventyConfig.addPassthroughCopy({ "src/assets/js": "/js" })
-  eleventyConfig.addPassthroughCopy({ "src/assets/images": "/img" })
-  eleventyConfig.addPassthroughCopy({ "src/assets/images/*.png": "/" })
-  eleventyConfig.addPassthroughCopy({ "src/assets/images/*.ico": "/" })
-  eleventyConfig.addPassthroughCopy({ "src/sass/": "/css" })
+  eleventyConfig.addPassthroughCopy({ "src/assets/styles/": "css" })
+  eleventyConfig.addPassthroughCopy({ "src/assets/scripts/": "js" })
 
   /** Add a filter to format inline dates for <time> tags */
-  eleventyConfig.addFilter("date", (date, format) => dayjs(date).format(format))
+  eleventyConfig.addJavaScriptFunction("date", (date, format) =>
+    dayjs(date).format(format)
+  )
 
   /* Filter to convert integer to ordinal, ie. 2 = 2nd */
-  eleventyConfig.addFilter("ordinal", (num) => ordinal(num))
+  eleventyConfig.addJavaScriptFunction("ordinal", (num) => ordinal(num))
+
+  /* Filter to convert integer to ordinal, ie. 2 = 2nd */
+  eleventyConfig.addJavaScriptFunction("words", (num) =>
+    converter.toWordsOrdinal(num)
+  )
 
   /* Filter to convert integer to comma number, ie. 1000 = 1,000 */
-  eleventyConfig.addFilter("commaNumber", (num) => commaNumber(num))
+  eleventyConfig.addJavaScriptFunction("commaNumber", (num) => commaNumber(num))
 
-  /* Filter to calculate distance between two points */
-  eleventyConfig.addShortcode("distance", (a, b, x, y) => {
-    let distance = new distFrom([a, b]).to([x, y]).in("mi")
-    return distance.toFixed(1)
-  })
-
-  /** Add a filter to format inline dates for <time> tags */
-  eleventyConfig.addFilter("ago", (year) =>
+  /** Add a filter to relative dates for <time> tags */
+  eleventyConfig.addJavaScriptFunction("ago", (year) =>
     dayjs().diff(dayjs(year, "YYYY"), "year")
   )
 
@@ -103,6 +72,7 @@ module.exports = function (eleventyConfig) {
     dir: {
       input: "src",
       output: "dist",
+      includes: "_templates",
     },
   }
 }
