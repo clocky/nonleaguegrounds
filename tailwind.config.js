6bb970aee05bf049a773322dea9f98a1171ae229
Mark McLaughlin <mark@mclaughlin.me.uk>
2023-05-29 23:02:08 -0700

    Define our own breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["src/**/*.webc"],
   theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+    },
     fontFamily: {
       display: ["Space Grotesk", "sans-serif"],
       mono: ["Cascadia Code", "monospace"],
