217ace6729ec42b5b5d36a307841bb25e73c3c5f
Mark McLaughlin <mark@mclaughlin.me.uk>
2023-06-14 20:53:33 -0700

    fix: Create better title and description tags

--- src/regions/ground.11tydata.js
+++ src/regions/ground.11tydata.js
@@ -1,4 +1,5 @@
 const slugify = require("slugify")
+const commaNumber = require("comma-number")
 
 module.exports = {
   layout: "base.webc",
@@ -9,8 +10,17 @@ module.exports = {
     alias: "ground",
   },
   eleventyComputed: {
-    title: ({ ground }) => ground.name,
+    title: ({ ground }) =>
+      `${ground.name} | ${ground.subOrganization[0].name} | ${ground.areaServed.name} `,
     permalink: ({ ground }) =>
       `regions/${ground.areaServed.slug.current}/${ground.slug.current}/index.html`,
+    description: ({ ground }) =>
+      `Explore ${ground.name}, home to the non-league football team, ${
+        ground.subOrganization[0].name
+      }. Located in ${
+        ground.address.addressRegion
+      }, this English football ground boasts a capacity of ${commaNumber(
+        ground.maximumAttendeeCapacity
+      )}.`,
   },
 }
